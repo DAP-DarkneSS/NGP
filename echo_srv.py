72d504f2ad277558e795308f38d9f33e95a79e20
Alexander <sapronov.alexander92@gmail.com>
2013-03-17 11:07:07 +0700

    Update echo_srv.py

--- echo_srv.py
+++ echo_srv.py
@@ -1,42 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
- 
+
 import socket
 from echo_stt import *
 import logging
-from os import makedirs, path
-from os.path import exists, getmtime
 from time import ctime
+import os
 
-data_path = path.dirname(__file__) + "/data/"
-
+# logging settings
 logging.basicConfig(filename='example.log',level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
- 
+
+# create empty dirs
+data_path = os.path.dirname(os.path.realpath(__file__)) + "/data/"
+if not os.path.exists(data_path):
+    os.makedirs(data_path)
+
+# default user
+id = 0;
+name = "Anonim"
+
+# create socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((host, port))
 s.listen(5)
+sock, addr = s.accept()
 
+#
 sms = "Starting on host " + str(host) + " at port " + str(port)
 print(sms)
 logging.debug(sms)
 
-sock, addr = s.accept()
-
-id = ""
-name = "Anonim"
-if not exists(data_path):
-    makedirs(data_path)
-f = open((data_path + name + '.txt'), 'a+')
-
 life = True
 while life:
     buf = sock.recv(1024)
-    logging.info(id + "-" + buf)
+    logging.info(name + " - " + buf)
 
     if buf == "exit":
         sock.send("bye")
-        sms = id + " became offline."
+        sms = name + " became offline."
         print(sms)
         logging.debug(sms)
         life = False
@@ -45,47 +47,53 @@ while life:
         sock.send("Nope!")
 
     elif buf == "GET":
+        f = open((data_path + name + '.txt'), 'a+')
         f.seek(0)
         i = f.read()
         if i != "":
             sock.send(i)
         else:
             sock.send("Nothing to show :(")
+        f.close()
 
     elif buf == "HELP":
-        sock.send("Доступные команды:\nADD\tсохранить информацию на сервере\nGET\tвывести сохранённую информацию\nHELP\tпоказать эту справку\nNAME\tавторизация\nME\tвывести имя текущего пользователя\nexit\tвыход.")
+        sock.send("Доступные команды:\nADD\tсохранить информацию на сервере\nGET\tвывести сохранённую информацию\nHELP\tпоказать эту справку\nSETNAME\tавторизация\nME\tвывести имя текущего пользователя\nexit\tвыход.")
 
     elif buf == "ME":
         sock.send(name)
 
     elif buf.startswith("ADD "):
         i = buf.split(" ", 1)
+        f = open((data_path + name + '.txt'), 'a+')
         f.seek(0, 2)
         f.write(i[1] + "\n")
+        f.close()
         sock.send("Done.")
 
-    elif buf.startswith("CID "):
-        i = buf.split(" ", 1)
-        id = i[1]
-        sms = id + " became online!"
+    elif buf.startswith("CONNECT"):
+        id = id + 1
+        sms = name + " became online!"
+        filename = data_path + name + '.txt'
+        f = open((data_path + name + '.txt'), 'a+')
+        f.close()
+
         print(sms)
         logging.debug(sms)
-        sock.send("Ты подключился к серверу МЕГАСЕРВЕР. Справка по командам доступка по запросу HELP")
+        sock.send("Ты подключился к серверу МЕГАСЕРВЕР. Справка по командам доступка по запросу HELP\nТвое имя - " + name)
 
-    elif buf.startswith("NAME "):
+    elif buf.startswith("SETNAME "):
         i = buf.split(" ", 1)
         name = i[1]
-
         filename = data_path + name + '.txt'
+
         try:
             if open(filename):
                 last_time = "\nYour last note was made at " + str(ctime(getmtime(filename)))
         except (IOError, NameError):
             last_time = ""
 
-        f.close()
         f = open((data_path + name + '.txt'), 'a+')
-
+        f.close()
         sock.send("Hello, " + name + "!" + last_time)
 
     elif buf:
